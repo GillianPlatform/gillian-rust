9b859d20b9720a8e17659d3dd3f5cd2f21fdb751
Sacha Ayoun <sachaayoun@gmail.com>
2021-05-04 18:02:04 +0200

    trying to get some mir

--- lib_rust_to_gil/src/config.rs
+++ lib_rust_to_gil/src/config.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub enum ExecMode {
     Concrete,
     Symbolic,
@@ -5,6 +7,18 @@ pub enum ExecMode {
     Act,
 }
 
+impl fmt::Debug for ExecMode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ExecMode::Concrete => write!(f, "concrete"),
+            ExecMode::Symbolic => write!(f, "symbolic"),
+            ExecMode::Verification => write!(f, "verification"),
+            ExecMode::Act => write!(f, "act"),
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct Config {
     pub mode: ExecMode,
 }
--- rust_to_gil/src/main.rs
+++ rust_to_gil/src/main.rs
@@ -5,7 +5,9 @@ extern crate rustc_errors;
 extern crate rustc_interface;
 extern crate rustc_lint;
 extern crate rustc_middle;
+extern crate rustc_hir;
 extern crate log;
+
 
 
 use lib_rtg::config::{Config as RtgConfig, ExecMode};
@@ -14,18 +16,26 @@ use simple_logger::SimpleLogger;
 use rustc_interface::interface::Compiler;
 use rustc_interface::Queries;
 use rustc_driver::Compilation;
+use rustc_hir::def_id::LOCAL_CRATE;
 
+#[derive(Debug)]
 struct RTGCompilerCalls {
     gil_config: RtgConfig,
 }
 
 impl rustc_driver::Callbacks for RTGCompilerCalls {
-    fn after_analysis<'tcx>(&mut self, compiler: &Compiler, _queries: &'tcx Queries<'tcx>) -> Compilation {
+    fn after_analysis<'tcx>(&mut self, compiler: &Compiler, queries: &'tcx Queries<'tcx>) -> Compilation {
         compiler.session().abort_if_errors();
         
-        if let ExecMode::Concrete = self.gil_config.mode {
-            println!("bite")
-        }
+        log::debug!("{:?}", self);
+        
+        queries.global_ctxt().unwrap().peek_mut().enter(|tcx| {
+            let key = tcx.mir_keys(LOCAL_CRATE).iter().next().unwrap();
+            log::debug!("{:?}", key);
+            let mir_main = tcx.mir_built(rustc_middle::ty::WithOptConstParam::unknown(*key));
+            log::debug!("{:?}", mir_main);
+            
+        });
         
         compiler.session().abort_if_errors();
         
@@ -48,7 +58,6 @@ impl Default for RTGCompilerCalls {
 fn main() {
     rustc_driver::install_ice_hook();
     SimpleLogger::new().init().unwrap();
-    log::debug!("bite");
     
     
     let args: Vec<_> = std::env::args().collect();
