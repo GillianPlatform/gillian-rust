86a57fe15e35ec747c48f0a7a839ef06d89c6870
Sacha Ayoun <sachaayoun@gmail.com>
2022-12-22 00:36:59 +0100

    add list_std_poly test for CI
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- test_all.sh
+++ test_all.sh
@@ -1,5 +1,5 @@
 cd Gillian-Rust
 esy x gillian-rust bulk-exec ../examples/concrete
 
-esy x gillian-rust verify ../examples/verification/list_std_mono.gil -a -l disabled
 esy x gillian-rust verify ../examples/verification/list_std_mono.rs -l disabled
+esy x gillian-rust verify ../examples/verification/list_std_poly.rs -l disabled
