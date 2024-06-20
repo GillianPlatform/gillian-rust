5991c71c35b93257b444caa98a287b02edecc173
Xavier Denis <xldenis@gmail.com>
2024-06-20 08:49:20 -0500

    Work on adding tests

--- tests/noproph/creusot_dummy.rs
+++ tests/noproph/creusot_dummy.rs
@@ -1,7 +1,23 @@
 extern crate creusillian;
 extern crate gilogic;
 
-#[creusillian::ensures(@ret == 4)]
+#[creusillian::ensures(ret == 4u32)]
 fn test() -> u32 {
     4
+}
+
+
+#[creusillian::ensures({let x = ret == 4u32; x})]
+fn test() -> u32 {
+    4
+}
+
+
+
+#[creusillian::ensures(match x { Some(a) => res == a, None => res == 0u32})]
+fn test(x : Option<u32>) -> u32 {
+    match x {
+        Some(x) => x,
+        None => 0,
+    }
 }
