ef302e3e5bcd22138d2915634fb087eae42c5e8d
Xavier Denis <denis.xavier@inf.ethz.chZ>
2024-06-24 11:11:33 +0200

    Compile basic specs

--- tests/noproph/creusot_dummy.rs
+++ tests/noproph/creusot_dummy.rs
@@ -8,14 +8,14 @@ fn test() -> u32 {
 
 
 #[creusillian::ensures({let x = ret == 4u32; x})]
-fn test() -> u32 {
+fn test2() -> u32 {
     4
 }
 
 
 
 #[creusillian::ensures(match x { Some(a) => res == a, None => res == 0u32})]
-fn test(x : Option<u32>) -> u32 {
+fn test3(x : Option<u32>) -> u32 {
     match x {
         Some(x) => x,
         None => 0,
