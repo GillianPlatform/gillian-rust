f79458e165fa0a3f76ecec9266124aa59b162a17
Xavier Denis <xldenis@gmail.com>
2024-07-05 16:35:12 +0200

    Add actual creusot tests

--- test_crates/sort/src/main.rs
+++ test_crates/sort/src/main.rs
@@ -6,14 +6,33 @@
 
 use creusillian::*;
 use gilogic::{RustAssertion, Seq, macros::*, prophecies::*};
-use std::marker::PhantomData;
-
-fn main() {
-    
-}
+use creusot_contracts::{Snapshot, snapshot, logic::IndexLogic};
 
 #[creusillian::requires((*x)@.len() == 0)]
 fn index_bad(x: &mut vec::Vec<i32>) {
     x.index_mut(0);
 }
 
+fn main() { }
+
+#[cfg_attr(gillian, trusted)]
+#[creusillian::ensures((^str)@.len() >= len@ && (^str)@.len() >= str@.len())]
+#[creusillian::ensures((^str)@.len() == len@ || (^str)@.len() == str@.len())]
+#[creusillian::ensures(forall<i: _> 0 <= i && i < ((^str)@.len() - str@.len()) ==> (^str)[i] == pad)]
+#[creusillian::ensures(forall<i: _> 0 <= i && i < str@.len() ==> (^str)[i + ((^str)@.len() - str@.len())] == str[i])]
+fn left_pad<T: Copy>(str: &mut Vec<T>, len: usize, pad: T) {
+    let old_str = creusot_contracts::snapshot! { str };
+    let mut c: Snapshot<_> = creusot_contracts::snapshot! { 0 };
+
+    #[creusot_contracts::invariant(^str == ^*old_str)]
+    #[creusot_contracts::invariant(old_str@.len() <= str@.len())]
+    #[creusot_contracts::invariant(old_str@.len() < len@ ==> str@.len() <= len@)]
+    #[creusot_contracts::invariant(str@.len() > len@ ==> str@.len() == old_str@.len())]
+    #[creusot_contracts::invariant(*c == str@.len() - old_str@.len())]
+    #[creusot_contracts::invariant(forall<i: _> *c <= i && i < str@.len() ==> str[i] == old_str[i - *c])]
+    #[creusot_contracts::invariant(forall<i: _> 0 <= i && i < *c ==> str[i] == pad)]
+    while str.len() < len {
+        str.insert(0, pad);
+        c = snapshot! { 1 + *c };
+    }
+}
